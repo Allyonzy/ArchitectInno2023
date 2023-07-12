cb93ecad21130cc527a150644ed331f716756848
Yelena Korneeva <yelena.korneeva@ya.ru>
2023-07-12 15:42:26 +0300

    Добавлена работа с миксинами

---
+++ interface_mixin/app.py
@@ -0,0 +1,70 @@
+import inspect
+
+class InnerDataMixin:
+    @property
+    def inner_data(self):
+        return inspect.getsource(self.__class__)
+    
+    @property
+    def dict_data(self):
+        return self.__dict__
+    
+
+class Fibonacci(InnerDataMixin):
+    __number = 0
+
+    def __init__(self, n = 0):
+        self.__n  = n
+    
+    def fib_recurs_by_index(self):
+        '''
+        basic solution
+        Рекурсивная функция для вычисления числа 
+        в последовательности Фибоначчи
+        1, 1, 2, 3, 5, 8, 13, 21, ....
+        @param int n - индекс в последовательности
+        @returns int result число Фибоначчи по заданному индексу
+        '''
+        if self.__n < 0 or self.__n == None : 
+            self.__number = None
+            return self.__number
+        if self.__n < 2: 
+            return self.__n
+        else:
+            self.__number = fib(self.__n-1) + fib(self.__n-2)
+            return self.__number
+    
+    def __str__(self):
+        return f"{self.__number}"
+    
+class Person(InnerDataMixin):
+    __age = 0
+
+    def __init__(self, name: str = 'Noname'):
+        self.name = name
+
+    @property
+    def age(self):
+        return self.__age
+    
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+    @staticmethod
+    def is_adult(age):
+        print(age)
+        if age > 18:
+            return "Взрослый"
+        else:
+            return "Меньше 18 лет"
+    
+if __name__ == '__main__':
+    fib = Fibonacci()
+    print(fib.inner_data)
+    print(fib.dict_data)
+
+    person = Person()
+    person.age = 30
+    print(person.inner_data)
+    print(person.dict_data)
---
+++ interface_mixin/test_app.py
@@ -0,0 +1,86 @@
+from app import InnerDataMixin
+
+from tkinter import *
+from tkinter import ttk
+
+def mixin(*mixin_classes, **mixin_kwargs): # decorator factory function
+    def decorator(cls): # decorator function
+        class wrapper(*mixin_classes, cls):
+            def __init__(self, *args, **kwargs):
+                wrapped_kwargs = mixin_kwargs.copy() # use the passed kwargs to update the
+                wrapped_kwargs.update(kwargs)        # mixin args, so caller can override
+                super().__init__(*args, **wrapped_kwargs)
+        # maybe modify wrapper's __name__, __qualname__, __doc__, etc. to match cls here?
+        return wrapper
+    return decorator
+
+class GraphicalEntity:
+    def __init__(self, pos_x: float = 0.0, pos_y: float = 0.0, size_x: float = 0, size_y: float = 0):
+        self.__pos_x = pos_x
+        self.__pos_y = pos_y
+        self.__size_x = size_x
+        self.__size_y = size_y
+
+    @property
+    def pos_x(self):
+        return self.__pos_x
+    
+    @property
+    def pos_y(self):
+        return self.__pos_y
+    
+    @property
+    def size_x(self):
+        return self.__size_x
+    
+    @property
+    def size_y(self):
+        return self.__size_y
+
+class ResizableMixin:
+    def resize(self, size_x, size_y):
+        self.size_x = size_x
+        self.size_y = size_y
+
+class ResizableGraphicalEntity(GraphicalEntity, ResizableMixin):
+    pass
+
+@mixin(InnerDataMixin)
+class Button(GraphicalEntity):
+    def __init__(self, pos_x=0.0, pos_y=0.0, size_x=0.0, size_y=0.0):
+        super().__init__(pos_x, pos_y, size_x, size_y)
+        print(self.dict_data)
+        self.__status = 'Enable'
+
+    @property
+    def status(self):
+        return self.__status
+
+    def toggle(self):
+        self.__status = 'Active'
+
+    def __str__(self):
+        return "Button (pos_x: {}, pos_y: {}, size_x: {}, size_y: {}, status: {})".format(
+            self.pos_x, 
+            self.pos_y, 
+            self.size_x, 
+            self.size_y, 
+            self.status
+            )
+    
+def tkinter_app():
+    root = Tk()
+    root.title("Test_desktop")
+    root.geometry("250x200")
+    button_03 = ttk.Button(text='Test')
+    button_03.pack()
+    
+    root.mainloop()
+
+if __name__=='__main__':
+    button_01 = Button()
+    print(button_01)
+    button_02 = Button(100, 200, 300, 100)
+    print(button_02)
+
+    tkinter_app()
