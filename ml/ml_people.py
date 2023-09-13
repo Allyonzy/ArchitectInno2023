9b03fb8de7a37b6b5f0c56be8da251d26909ce09
Allyonzy <64588181+Allyonzy@users.noreply.github.com>
2023-09-13 15:20:53 +0300

    Тестирование ml (пока без flask)

--- ml/ml_people.py
+++ ml/ml_people.py
@@ -1,6 +1,6 @@
 # загрузка библиотек
 import pandas as pd
-import random
+import numpy as np
 
 from sklearn.preprocessing import LabelEncoder
 
@@ -16,9 +16,73 @@ from sklearn.metrics import accuracy_score
 #работа с моделью в браузере (подключение)
 from flask import Flask, request, jsonify
 
+#TODO добавить Flask
+
+# Набор констант по программе
+MODEL_FILE_EFF = 'people_university_model_eff.cbm'
+MODEL_FILE = 'people_university_model.cbm'
+DF_COUNT = 2001
+SUBJ_COUNT = 6
+MIN_GRADE = 0
+MAX_GRADE = 99
+FINAL_RESULT_LIST = ['удовлетворительно', 'хорошо', 'отлично']
+
 # Набор def и классов
-# Функция для классификации новой квартиры
+
+def generate_students():
+    '''
+    Создайте датасет из не менее чем 2000 записей, содержащий данные о
+    среднем балле студентов (от 0 до 99) по 6 предметам и оценка итоговой
+    лабораторной работы (удовлетворительно, хорошо, отлично).
+
+    Строки датасета:
+    id # идентификатор студента
+    subjects = [{'math': score1}, {'reading': score2}, {'ml': score3}, {'physics': score4}, {'optimisation': score4}, {'sports': score5}] # список предметов
+    final_lab = ['удовлетворительно', 'хорошо', 'отлично'] # итоговая работа
+
+    Варианты получения: 
+    1. Преобразовать существующий датасет
+    2. Сгенерировать (random python)
+
+    Создать из датасета DataFrame
+    '''
+    def generate_final(row):
+        # Балл ниже 30 - удовл
+        cond_c = row['mean_score'] < 50
+        # Балл 50 - 70 - хор
+        cond_b = row['mean_score'] >= 50 
+        cond_b_1 = row['mean_score'] < 60
+        # Балл 70 - 100 - отл
+        cond_a = row['mean_score'] >= 60
+        val = ''
+        if(cond_c):
+            val = FINAL_RESULT_LIST[0]
+        elif(cond_b and cond_b_1):
+            val = FINAL_RESULT_LIST[1]
+        else:
+            val = FINAL_RESULT_LIST[2]
+        # return FINAL_RESULT_LIST[0] if cond_c else (FINAL_RESULT_LIST[1] if cond_b else FINAL_RESULT_LIST[2])
+        return val
+
+    subjects = [f"subject_{num + 1}" for num in range(SUBJ_COUNT)]
+
+    grades = np.random.randint(MIN_GRADE, MAX_GRADE + 1, (DF_COUNT, SUBJ_COUNT))
+    student_scores = pd.DataFrame (grades, columns = subjects)
+    
+    student_scores.sum(axis=0)
+    student_scores['mean_score'] = np.round((student_scores.sum(axis=1)/SUBJ_COUNT), 3)
+    print(student_scores.head(5))
+    
+    student_scores['final_lab'] = student_scores.apply(generate_final, axis=1)
+    return student_scores
+
+
+
 def classifyStudent(new_student):
+    '''
+    Функция для классификации студентов
+    @param new_student - массив ключ-значение или строка в датасете
+    '''
     # Чтение обученной модели из файла
     model = CatBoostClassifier()
     model.load_model(MODEL_FILE)
@@ -35,24 +99,10 @@ def classifyStudent(new_student):
 
     return predicted_category
 
-#1. Входные данные (загрузка/генерация)
+# df = generate_students()
+# df.to_csv('ml/student.csv', index=False)
+
 '''
-Создайте датасет из не менее чем 2000 записей, содержащий данные о
-среднем балле студентов (от 0 до 99) по 6 предметам и оценка итоговой
-лабораторной работы (удовлетворительно, хорошо, отлично).
-Названия предметов, средний балл, оценка  итоговой лабораторной работы задается произвольно 
-на ваше усмотрение.
-
-Строки датасета:
-id # идентификатор студента
-subjects = [{'math': score1}, {'reading': score2}, {'ml': score3}, {'physics': score4}, {'optimisation': score4}, {'sports': score5}] # список предметов
-final_lab = ['удовлетворительно', 'хорошо', 'отлично'] # итоговая работа
-
-Варианты получения: 
-1. Преобразовать существующий датасет
-2. Сгенерировать (random python)
-
-Создать из датасета DataFrame
 
 Разделить на X и y
 X - все предметы + *студент*
@@ -62,16 +112,14 @@ id студента убрать из X
 y - результирующий столбец, Final lab <- LabelEncoder()
 
 '''
-
-# 1. pd.csv() - если прочитать файл с данными
-# df_student = pd.DataFrame() #сгенерировать массив студентов
-
-subjects = [{'math': score1}, {'reading': score2}, {'ml': score3}, {'physics': score4}, 
-            {'optimisation': score4}, {'sports': score5}] # список предметов
-
-X = df_student[['math', 'reading', 'ml', 'physics', 'optimisation', 'sports']]
+df = pd.read_csv('ml/student.csv')
+X = df.drop(columns=['final_lab', 'mean_score'], axis=1)
+print(X)
+y = df['final_lab']
+print(y)
 label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(df_student['final_lab'])
+y = label_encoder.fit_transform(y)
+print(y)
 
 # 2. Выберите лучшие 3 признака для обучения
 selector = SelectKBest(chi2, k=2)
@@ -81,17 +129,22 @@ X_new = selector.fit_transform(X, y)
 # 80% обучающей выборки, 20% тестовой
 X_train_eff, X_test_eff, y_train_eff, y_test_eff = train_test_split(X_new, y, test_size=0.2, random_state=1234)
 
+
 # *3.1* Разбейте датасет на тестовую и обучающую выборку train_test_split (для всех признаков)
 # 80% обучающей выборки, 20% тестовой
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
-# 4. Проведите обучение модели, результатом должна быть сохраненная модель.
-# Обучаем модель CatBoostClassifier
-model_eff = CatBoostClassifier(iterations=100, depth=4, learning_rate=0.1, cat_features=cat_features)
+# # 4. Проведите обучение модели, результатом должна быть сохраненная модель.
+# # Обучаем модель CatBoostClassifier
+model_eff = CatBoostClassifier(iterations=100, depth=4, learning_rate=0.1)
 model_eff.fit(X_train_eff, y_train_eff)
+# Сохранение модели в файл
+model_eff.save_model(MODEL_FILE_EFF)
 
-model = CatBoostClassifier(iterations=100, depth=4, learning_rate=0.1, cat_features=cat_features)
+model = CatBoostClassifier(iterations=100, depth=4, learning_rate=0.1)
 model.fit(X_train, y_train)
+# Сохранение модели в файл
+model_eff.save_model(MODEL_FILE)
 
 # 5. Проведите тестирование модели, результатом должно быть число - точность модели (accuracy) на тестовой выборке.
 # Делаем предсказания на тестовом наборе
@@ -106,9 +159,9 @@ print(f"Точность модели: {accuracy_eff}")
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Точность модели: {accuracy}")
 
-# Сохранить модель
+# # Сохранить модель
 
-# 6. Реализуйте функцию, которая на вход принимает оценку студента по 6 предметам и возвращает прогноз оценки.
+# # 6. Реализуйте функцию, которая на вход принимает оценку студента по 6 предметам и возвращает прогноз оценки.
 
-# генерация новых данных new_student
-# прогноз оценки через classifyStudent(new_student)
+# # генерация новых данных new_student
+# # прогноз оценки через classifyStudent(new_student)
