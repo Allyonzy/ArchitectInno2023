77bf9d265227e4af9d7bc68ebacd5a1724969747
Allyonzy <64588181+Allyonzy@users.noreply.github.com>
2023-09-14 21:47:03 +0300

    Доработка модульности

--- ml/ml_people.py
+++ ml/ml_people.py
@@ -52,9 +52,9 @@ def generate_students():
     def generate_final(row):
         # TODO поменять random
         # Балл ниже 30 - удовл
-        cond_c = row['mean_score'] < 30
-        # Балл 50 - 60 - хор
-        cond_b = row['mean_score'] >= 50 
+        cond_c = row['mean_score'] < 35
+        # Балл 35 - 60 - хор
+        cond_b = row['mean_score'] >= 35 
         cond_b_1 = row['mean_score'] < 60
         # Балл 60 - 100 - отл
         cond_a = row['mean_score'] >= 60
@@ -83,17 +83,17 @@ def generate_students():
 
 
 
-def classifyStudent(new_student):
+def classifyStudent(new_student, new_columns, model_file, categories=[0, 1, 2]):
     '''
     Функция для классификации студентов
     @param new_student - массив ключ-значение или строка в датасете
     '''
     # Чтение обученной модели из файла
     model = CatBoostClassifier()
-    model.load_model(MODEL_FILE)
+    model.load_model(model_file)
 
     # Преобразование данных студента в DataFrame
-    new_data = pd.DataFrame([new_student])
+    new_data = pd.DataFrame([new_student], columns=new_columns)
 
     # Использование модели для предсказания
     predicted_category = model.predict(new_data)[0]
@@ -104,88 +104,100 @@ def classifyStudent(new_student):
 
     return predicted_category
 
-# df = generate_students()
-# df.to_csv('ml/student.csv', index=False)
+def create_new_dataset():
+    df = generate_students()
+    df.to_csv('ml/student.csv', index=False)
 
-'''
+def preprocess_dataset(
+            file='ml/student.csv', 
+            to_drop=['final_lab', 'mean_score'], 
+            y_name='final_lab',
+            need_to_encode_y=True,
+            need_best_features=True,
+            test_size=0.2,
+            random_state=1234
+        ):
+    '''
+    Разделить на X и y
+    X - все предметы + *студент*
 
-Разделить на X и y
-X - все предметы + *студент*
+    id студента убрать из X
 
-id студента убрать из X
+    y - результирующий столбец, Final lab <- LabelEncoder()
 
-y - результирующий столбец, Final lab <- LabelEncoder()
+    '''
+    df = pd.read_csv(file)
+    X = df.drop(columns=to_drop, axis=1)
+    print(X)
+    y = df[y_name] #Series
+    print(y)
 
-'''
-df = pd.read_csv('ml/student.csv')
-X = df.drop(columns=['final_lab', 'mean_score'], axis=1)
-# X = df.drop(columns=['final_lab'], axis=1)
-print(X)
-y = df['final_lab'] #Series
-print(y)
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(y)
-print(y)
+    if (need_to_encode_y):
+        label_encoder = LabelEncoder()
+        temp = label_encoder.fit_transform(y)
+        new_result = pd.DataFrame([y, temp], columns=['y', 'encoded_y'])
+        new_result.to_csv('ml/result_labels.csv', index=False)
+        y = temp       
+        print(y)
 
-# 2. Выберите лучшие 3 признака для обучения
-selector = SelectKBest(chi2, k=3)
-X_new = selector.fit_transform(X, y)
+    if (need_best_features):
+        # 2. Выберите лучшие 3 признака для обучения
+        selector = SelectKBest(chi2, k=3)
+        X = selector.fit_transform(X, y)
 
-# 3. Разбейте датасет на тестовую и обучающую выборку train_test_split (для лучших признаков)
-# 80% обучающей выборки, 20% тестовой
-X_train_eff, X_test_eff, y_train_eff, y_test_eff = train_test_split(X_new, y, test_size=0.2, random_state=1234)
+    # 3. Разбейте датасет на тестовую и обучающую выборку train_test_split (для лучших признаков)
+    # 80% обучающей выборки, 20% тестовой
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
+    return X_train, X_test, y_train, y_test
 
-# *3.1* Разбейте датасет на тестовую и обучающую выборку train_test_split (для всех признаков)
-# 80% обучающей выборки, 20% тестовой
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+def train_model(X_train, y_train, iterations=100, depth=4, learning_rate=0.1, file_to_save=MODEL_FILE):
+    # # 4. Проведите обучение модели, результатом должна быть сохраненная модель.
+    # # Обучаем модель CatBoostClassifier
+    model = CatBoostClassifier(iterations=iterations, depth=depth, learning_rate=learning_rate)
+    model.fit(X_train, y_train)
+    # Сохранение модели в файл
+    model.save_model(file_to_save)
 
-# # 4. Проведите обучение модели, результатом должна быть сохраненная модель.
-# # Обучаем модель CatBoostClassifier
-model_eff = CatBoostClassifier(iterations=100, depth=4, learning_rate=0.1)
-model_eff.fit(X_train_eff, y_train_eff)
-# Сохранение модели в файл
-model_eff.save_model(MODEL_FILE_EFF)
+def model_report(model, X_test, y_test, average='weighted'):
+    # 5. Проведите тестирование модели, результатом должно быть число - точность модели (accuracy) на тестовой выборке.
+    # Делаем предсказания на тестовом наборе
+    y_pred = model.predict(X_test)
 
-model = CatBoostClassifier(iterations=100, depth=4, learning_rate=0.1)
-model.fit(X_train, y_train)
-# Сохранение модели в файл
-model_eff.save_model(MODEL_FILE)
+    # Оцениваем точность модели
+    accuracy_eff = accuracy_score(y_test, y_pred)
+    print(f"Правильность (accuracy) модели: {accuracy_eff}")
 
-# 5. Проведите тестирование модели, результатом должно быть число - точность модели (accuracy) на тестовой выборке.
-# Делаем предсказания на тестовом наборе
-y_pred_eff = model_eff.predict(X_test_eff)
+    precision_eff = precision_score(y_test, y_pred, average=average)
+    print(f"Точность (precision) модели: {precision_eff}")
 
-y_pred = model.predict(X_test)
+    recall_eff = recall_score(y_test, y_pred, average=average)
+    print(f"Полнота (recall) модели: {recall_eff}")
 
-# Оцениваем точность модели
-accuracy_eff = accuracy_score(y_test_eff, y_pred_eff)
-print(f"Правильность (accuracy) модели: {accuracy_eff}")
+    f1_eff = f1_score(y_test, y_pred, average=average)
+    print(f"F1 мера модели: {f1_eff}")
 
-precision_eff = precision_score(y_test_eff, y_pred_eff, average='weighted')
-print(f"Точность (precision) модели: {precision_eff}")
+# #Натренировать модель
+# X_train, X_test, y_train, y_test = preprocess_dataset()
+# train_model(X_train, y_train)
+# new_model = CatBoostClassifier()
+# new_model.load_model(MODEL_FILE)
+# model_report(new_model, X_test, y_test, average='weighted')
 
-recall_eff = recall_score(y_test_eff, y_pred_eff, average='weighted')
-print(f"Полнота (recall) модели: {recall_eff}")
-
-f1_eff = f1_score(y_test_eff, y_pred_eff, average='weighted')
-print(f"F1 мера модели: {f1_eff}")
-
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Точность модели: {accuracy}")
-
-precision = precision_score(y_test, y_pred, average='weighted')
-print(f"Точность (precision) модели: {precision}")
-
-recall = recall_score(y_test, y_pred, average='weighted')
-print(f"Полнота (recall) модели: {recall}")
-
-f1 = f1_score(y_test, y_pred, average='weighted')
-print(f"F1 мера модели: {f1}")
 
 # # Сохранить модель
 
 # # 6. Реализуйте функцию, которая на вход принимает оценку студента по 6 предметам и возвращает прогноз оценки.
+
+new_grades = np.random.randint(MIN_GRADE, MAX_GRADE + 1, SUBJ_COUNT)
+print(new_grades)
+result = classifyStudent(new_grades, MODEL_FILE)
+
+print(f"Студент скорее всего получит: {result}")
+
+result = classifyStudent(new_grades, MODEL_FILE_EFF)
+
+print(f"Студент скорее всего получит (если была модель с SelectBestK): {result}")
 
 # # генерация новых данных new_student
 # # прогноз оценки через classifyStudent(new_student)
